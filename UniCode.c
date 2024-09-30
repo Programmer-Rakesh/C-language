347600f2a4c92e215c4bc9a090ad30b95123508d
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-10-01 00:28:19 +0530

    New Files

---
+++ UniCode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    printf("Department ECE\n");
+    printf("Galgotias Univeristy\n");
+
+    int a = 5;
+    float b = 2.5;
+    printf("value of a=%d", a);
+    printf("value of b=%f", b);
+    return 0;
+}
